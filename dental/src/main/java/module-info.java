60e4ace9dc221e1fd100fd7db27272b4846756be
Gydo kosten <gydo.kosten@gmail.com>
2023-02-03 17:28:24 +0100

    tablevies

--- dental/src/main/java/module-info.java
+++ dental/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module nl.gkosten.adainf {
     requires javafx.controls;
     requires java.sql;
     exports nl.gkosten.adainf;
+
+    opens nl.gkosten.adainf.models to javafx.base;
 }
--- dental/src/main/java/nl/gkosten/adainf/views/BehandelingenOverzicht.java
+++ dental/src/main/java/nl/gkosten/adainf/views/BehandelingenOverzicht.java
@@ -1,8 +1,18 @@
 package nl.gkosten.adainf.views;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import nl.gkosten.adainf.App;
+import nl.gkosten.adainf.datalayer.DAOProvider;
+import nl.gkosten.adainf.datalayer.DatalayerException;
+import nl.gkosten.adainf.models.Behandeling;
 
 public class BehandelingenOverzicht {
     private Scene behandelingenOverzichtScene;
@@ -12,6 +22,47 @@ public class BehandelingenOverzicht {
         container = new VBox();
         container.setPrefWidth(App.PREFERRED_DIMENSIONS_X);
         container.setPrefHeight(App.PREFERRED_DIMENSIONS_Y);
+
+
+        Text title = new Text("Behandelingen");
+        container.getChildren().add(title);
+
+        TableView behandelingenTable = new TableView();
+
+        TableColumn codeColumn = new TableColumn("Code");
+        codeColumn.setCellValueFactory(
+                new PropertyValueFactory<>("code")
+        );
+
+        TableColumn omschrijvingColumn = new TableColumn("Omschrijving");
+        omschrijvingColumn.setCellValueFactory(
+                new PropertyValueFactory<>("omschrijving")
+        );
+
+        TableColumn prijsColumn = new TableColumn("Prijs");
+        prijsColumn.setCellValueFactory(
+                new PropertyValueFactory<>("prijs")
+        );
+
+        behandelingenTable.getColumns().addAll(
+                codeColumn,
+                omschrijvingColumn,
+                prijsColumn
+        );
+
+        ObservableList<Behandeling> behandelingen = FXCollections.observableArrayList();
+
+        try {
+            behandelingen = FXCollections.observableArrayList(
+                    DAOProvider.getBehandelingDAO().getAllBehandelingen()
+            );
+        } catch (DatalayerException e) {
+            throw new RuntimeException(e);
+        }
+
+        behandelingenTable.setItems(behandelingen);
+
+        container.getChildren().add(behandelingenTable);
 
         behandelingenOverzichtScene = new Scene(container);
     }
