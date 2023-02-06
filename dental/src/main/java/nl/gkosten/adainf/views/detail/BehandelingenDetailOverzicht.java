d185d2e90fd0bc901c724765b6d0ed3f609a4ddc
Gydo kosten <gydo.kosten@gmail.com>
2023-02-06 17:23:54 +0100

    form inputs

--- dental/src/main/java/nl/gkosten/adainf/views/detail/BehandelingenDetailOverzicht.java
+++ dental/src/main/java/nl/gkosten/adainf/views/detail/BehandelingenDetailOverzicht.java
@@ -1,6 +1,10 @@
 package nl.gkosten.adainf.views.detail;
 
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import nl.gkosten.adainf.App;
@@ -20,6 +24,30 @@ public class BehandelingenDetailOverzicht {
         Text title = new Text(String.format("Behandeling '%s'", behandeling.getCode()));
         container.getChildren().add(title);
 
+        Label codeLabel = new Label("Behandelingscode:");
+        Label prijsLabel = new Label("Prijs:");
+        Label omschrijvingLabel = new Label("Omschrijving:");
+
+        TextField codeField = new TextField();
+        TextField prijsField = new TextField();
+        TextField omschrijvingField = new TextField();
+
+        Button updateButton = new Button("Opslaan");
+        Button deleteButton = new Button("Verwijderen");
+
+        GridPane formGrid = new GridPane();
+        formGrid.setPrefWidth(App.PREFERRED_DIMENSIONS_X);
+
+        formGrid.add(codeLabel,             0, 0);
+        formGrid.add(codeField,             1, 0);
+        formGrid.add(prijsLabel,            0, 1);
+        formGrid.add(prijsField,            1, 1);
+        formGrid.add(omschrijvingLabel,     0, 2);
+        formGrid.add(omschrijvingField,     1, 2);
+        formGrid.add(updateButton,          0, 3);
+        formGrid.add(deleteButton,          1, 3);
+
+        container.getChildren().add(formGrid);
 
 
     }
